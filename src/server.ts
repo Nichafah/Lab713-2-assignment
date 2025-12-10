fd1d3050129613050a3bb4271b518ca9689d7ae5
Nichafah <nichapat_koth@cmu.ac.th>
2025-12-10 13:02:38 +0700

    Task7

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 interface Book {
+    id: number;
     title: string;
     author_name: string;
     description: string;
@@ -11,12 +12,14 @@ interface Book {
 
 const books: Book[] = [
     {
+        id: 1,
         title: "Clean Code",
         author_name: "Robert C. Martin",
         description: "A handbook of agile software craftsmanship",
         group: "Programming",
     },
     {
+        id: 2,
         title: "Atomic Habits",
         author_name: "James Clear",
         description: "How to build good habits",
@@ -25,22 +28,33 @@ const books: Book[] = [
 ];
 
 // GET /books (search by title prefix)
-app.get("/books", (req: Request, res: Response) => {
-    const titleQuery = req.query.title as string;
+// app.get("/books", (req: Request, res: Response) => {
+//     const titleQuery = req.query.title as string;
+//
+//     if (titleQuery) {
+//         // ค้นหาชื่อที่ขึ้นต้นด้วยคำค้นแบบไม่สนตัวพิมพ์เล็กใหญ่
+//         const filteredBooks = books.filter(book =>
+//             book.title.toLowerCase().startsWith(titleQuery.toLowerCase())
+//         );
+//         res.json(filteredBooks);
+//     } else {
+//         // ถ้าไม่ส่ง query parameter ให้คืนทั้งหมด
+//         res.json(books);
+//     }
+// });
 
-    if (titleQuery) {
-        // ค้นหาชื่อที่ขึ้นต้นด้วยคำค้นแบบไม่สนตัวพิมพ์เล็กใหญ่
-        const filteredBooks = books.filter(book =>
-            book.title.toLowerCase().startsWith(titleQuery.toLowerCase())
-        );
-        res.json(filteredBooks);
+
+app.get("/books/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const book = books.find((b) => b.id === id);
+
+    if (book) {
+        res.json(book);
     } else {
-        // ถ้าไม่ส่ง query parameter ให้คืนทั้งหมด
-        res.json(books);
+        res.status(404).send("Book not found");
     }
 });
-
-
 
 
 // Start server
